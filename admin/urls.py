16390676b17ca9be8f1c2dfeed4850dde7a5cb3d
luopeng <peng.luo@idiaoyan.com>
2016-06-19 23:16:52 +0800

    update 排序和查询

--- admin/urls.py
+++ admin/urls.py
@@ -4,6 +4,7 @@ urlpatterns = [
     (r"/admin/?", views.AdminHandler),
     (r"/admin/login/?", views.LoginHandler),
     (r"/admin/logout/?", views.LogoutHandler),
+    (r"/admin/user_list/?", views.AdminUserListHandler),
     (r"/admin/add_user/?", views.AddUserHandler),
     (r"/admin/check_user/?", views.CheckUserHandler),
 ]
--- admin/views.py
+++ admin/views.py
@@ -39,11 +39,36 @@ class LogoutHandler(AdminBaseHandler):
 
 class AdminHandler(AdminBaseHandler):
     def get(self):
+        self.render('admin/admin.html')
+
+
+class AdminUserListHandler(AdminBaseHandler):
+    def get(self):
+        sort = self.get_argument('sort', '-created')
+        search = self.get_argument('search', '')
+        search_value = self.get_argument('search_value', '')
         admin_user = self.get_secure_cookie('admin_user')
         if not admin_user:
             self.redirect('/admin/login/')
             return
-        user_list = auth_dao.get_user_list()
+        user_list = auth_dao.get_user_list().order_by(sort)
+        if search == 'name':
+            user_list = user_list.filter(name=search_value)
+        elif search == 'card_id':
+            user_list = user_list.filter(card_id=search_value)
+        elif search == 'mobile':
+            user_list = user_list.filter(mobile=search_value)
+        elif search == 'email':
+            user_list = user_list.filter(email=search_value)
+        elif search == 'perm':
+            user_list = user_list.filter(perm=search_value)
+        elif search == 'status':
+            user_list = user_list.filter(status=search_value)
+        elif search == 'department':
+            user_list = user_list.filter(department=search_value)
+        elif search == 'position':
+            user_list = user_list.filter(position=search_value)
+
         user_status_dict = auth_enums.USER_STATUS_DICT
         self.render('admin/user_list.html', user_list=user_list, user_status_dict=user_status_dict)
 
@@ -108,9 +133,21 @@ class CheckUserHandler(AdminBaseHandler):
     def post(self):
         check_user = self.get_argument('check_user', '')
         card_id = self.get_argument('card_id', '')
-        print 'aaaa:', card_id
         if check_user == 'pass':
-            auth_dao.update_user_by_card_id(card_id, {'status': auth_enums.USER_STATUS_NORMAL})
+            status = auth_enums.USER_STATUS_NORMAL
+        elif check_user == 'active':
+            status = auth_enums.USER_STATUS_NORMAL
+        elif check_user == 'nopass':
+            status = auth_enums.USER_STATUS_NOPASS
+        elif check_user == 'forbid':
+            status = auth_enums.USER_STATUS_FORBID
+        elif check_user == 'delete':
+            status = auth_enums.USER_STATUS_DELETE
+        else:
+            self.write(json.dumps({'status': 'fail', 'err_msg': 'args mistake'}))
+            return
+
+        auth_dao.update_user_by_card_id(card_id, {'status': status})
         self.write(json.dumps({'status': 'ok'}))
         return
 
