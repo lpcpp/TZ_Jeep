86122e2f139f8cb2dc01c5925579bb0d83c8297a
luopeng <peng.luo@idiaoyan.com>
2017-04-28 17:52:32 +0800

    视频列表页布局

--- index.py
+++ index.py
@@ -5,21 +5,5 @@ from auth import enums as auth_enums
 
 
 class IndexHandler(BaseHandler):
-
-    @tornado.web.authenticated
     def get(self):
-        user = self.get_current_user()
-        user = auth_dao.get_user(user)
-        if user.status != auth_enums.USER_STATUS_NORMAL:
-            self.write('your account is checking')
-            return
-
-        perm = ''
-        if user.perm == auth_enums.ACADEMY:
-            perm = auth_enums.ACADEMY
-        if user.perm == auth_enums.BUSINESS:
-            perm = auth_enums.BUSINESS
-        if user.perm == auth_enums.CLUSTER:
-            perm = auth_enums.CLUSTER
-
-        self.render('index.html', perm=perm)
+        self.render('index.html')
